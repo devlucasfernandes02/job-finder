f204ffcec72ff229b0c7df5bcc65f5799d3cdd10
Lucas-Fernandes02 <alfds10@hotmail.com>
2024-07-09 13:54:30 -0300

    fim

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app        = express();
 const path       = require('path');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
+const Job        = require('./models/Job');
+const Sequelize  = require('sequelize');
+const Op         = Sequelize.Op;
 
 const PORT = 3000;
 
@@ -36,7 +39,35 @@ db
 
 // routes
 app.get('/', (req, res) => {
-    res.render('index');
+
+    let search = req.query.job;
+    let query = '%'+search+'%'; // PH -> PHP, Word -> Wordpress, press -> Wordpress
+
+    if(!search) {
+        Job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+
+            res.render('index', {
+                jobs
+            });
+
+        })
+        .catch(err => console.og(err));
+    } else {
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+
+            res.render('index', {
+                jobs, search
+            });
+        });
+    }
 });
 
 // jobs routes
